f243a7bec390669ff23735be8903a5886dfd982a
FlorianGrohmann <florian-grohmann@gmx.de>
2023-02-11 18:58:15 +0100

    manychange2

--- Hackathon/ModelKeras/Species_FHHED.py
+++ Hackathon/ModelKeras/Species_FHHED.py
@@ -1,13 +1,31 @@
 import os
 import pandas as pd
-path_input_data = os.path.join('.', 'test_combined_Species.csv')
+import pickle as pk
 
-df_data = pd.read_csv(path_input_data, delimiter=',')
-df_data.drop(df_data.columns[0], axis=1, inplace=True)
+path_input_data = os.path.join('.', 'train_combined_Species.csv')
+
+df = pd.read_csv(path_input_data, delimiter=',')
+df.drop(df.columns[0], axis=1, inplace=True)
 
 ##########################
-# You do the preprocessing here
+df = df.fillna(0)
+
+inputvalues = df.drop(['label'], axis=1)
+outputvalues = df['label']
+X, y = inputvalues.values, outputvalues.values
+X = X.astype('float32')
+
+scaler = pk.load(open("scaler.pkl",'rb'))
+X = scaler.transform(X)
+
+pca = pk.load(open("pca.pkl",'rb'))
+X = pca.transform(X)
+
+X = pd.DataFrame(X)
+y = pd.DataFrame(y, columns=['label'])
+df = pd.concat([X, y], axis=1)
+print(df)
 ##########################
 
-path_output_data = path_input_data
-df_data.to_csv(path_output_data)
+#path_output_data = path_input_data
+#df.to_csv(path_output_data)
